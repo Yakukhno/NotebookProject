71f34a6a8b1bc89d461152d4e22816f8bd9db17f
Ivan Yakukhno <sokole2@yandex.ru>
2016-11-06 18:02:40 +0200

    added models of group and note

---
+++ src/main/java/ua/training/models/Group.java
@@ -0,0 +1,10 @@
+package ua.training.models;
+
+/**
+ * Created by Ivan on 06.11.2016.
+ */
+public enum Group {
+
+    FRIENDS, RELATIVES, COLLEAGUES, OTHER
+
+}
---
+++ src/main/java/ua/training/models/Note.java
@@ -0,0 +1,145 @@
+package ua.training.models;
+
+import java.util.Date;
+
+/**
+ * Created by Ivan on 06.11.2016.
+ */
+public class Note {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String fullName;
+    private String username;
+    private String comment;
+    private Group group;
+    private String homePhone;
+    private String cellPhone;
+    private String cellPhoneOptional;
+    private String email;
+    private String skype;
+    private String address;
+    private Date initialDate;
+    private Date lastChangeDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public String getCellPhoneOptional() {
+        return cellPhoneOptional;
+    }
+
+    public void setCellPhoneOptional(String cellPhoneOptional) {
+        this.cellPhoneOptional = cellPhoneOptional;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSkype() {
+        return skype;
+    }
+
+    public void setSkype(String skype) {
+        this.skype = skype;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getInitialDate() {
+        return initialDate;
+    }
+
+    public void setInitialDate(Date initialDate) {
+        this.initialDate = initialDate;
+    }
+
+    public Date getLastChangeDate() {
+        return lastChangeDate;
+    }
+
+    public void setLastChangeDate(Date lastChangeDate) {
+        this.lastChangeDate = lastChangeDate;
+    }
+}
